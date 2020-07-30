c421c4520a3d3c3a3ff1e32759dadb605db6e915
rethenya <61034898+rethenya@users.noreply.github.com>
2020-07-30 16:37:48 +0530

    Add files via upload

---
+++ flask1.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,Response,jsonify
+
+app=Flask(__name__)
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'year_published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'year_published': '1975'}
+]
+
+@app.route('/')
+def users():
+    return jsonify(books)
+
+if __name__=='__main__':
+  app.run()
